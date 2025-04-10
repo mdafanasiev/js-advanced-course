a3a84913646a9d4cfea586cc3e57e8880da6c32f
mdafanasiev <maxaf278@gmail.com>
2025-04-10 19:41:51 +0300

    доработки для дз 7 модуля

--- 5-oop/app.js
+++ 5-oop/app.js
@@ -15,27 +15,27 @@ Character.prototype.talk = function () {
 const Orc = function (race, name, language, weapon) {
     Character.call(this, race, name, language);
     this.weapon = weapon;
-    this.hit = function () {
-        console.log(`${this.race} ${this.name} strikes with a ${this.weapon}`);
-    };
 };
 
 Orc.prototype = Object.create(Character.prototype);
 Orc.prototype.constructor = Orc;
+Orc.prototype.hit = function () {
+    console.log(`${this.race} ${this.name} strikes with a ${this.weapon}`);
+};
 
 const Elf = function (race, name, language, spells) {
     Character.call(this, race, name, language);
     this.spells = spells;
-    this.createSpell = function () {
-        const spellIndex = Math.floor(Math.random() * this.spells.length);
-        console.log(
-            `${this.race} ${this.name} created a ${this.spells[spellIndex]}`
-        );
-    };
 };
 
 Elf.prototype = Object.create(Character.prototype);
 Elf.prototype.constructor = Elf;
+Elf.prototype.createSpell = function () {
+    const spellIndex = Math.floor(Math.random() * this.spells.length);
+    console.log(
+        `${this.race} ${this.name} created a ${this.spells[spellIndex]}`
+    );
+};
 
 const orcVasya = new Orc("Orc", "Vasiliy", "russian", "Axe");
 orcVasya.hit();
